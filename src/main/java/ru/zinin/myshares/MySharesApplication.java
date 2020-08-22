3069830a6b4bbdf4f53cabc9ffaa228bc50bfb68
daboggg <vzinin@list.ru>
2020-08-22 15:30:47 +0300

    init

---
+++ src/main/java/ru/zinin/myshares/MySharesApplication.java
@@ -0,0 +1,13 @@
+package ru.zinin.myshares;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySharesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySharesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/zinin/myshares/MySharesApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.zinin.myshares;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySharesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
